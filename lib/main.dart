a22e2d7f9b7c5b8b6824886d70a176a826c0e220
Rahul rv <rahulrv001@gmail.com>
2025-07-21 18:21:45 +0530

    Added the firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/app/view/app.dart';
+import 'package:notes/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
-
